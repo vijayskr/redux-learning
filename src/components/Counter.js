4b4e1b68d9742a89f168e9f663df2ee0346a60fe
Vijayasankar <vijayasankar@outlook.com>
2021-05-15 23:14:56 -0400

    Added REDUX REACT-REDUX Dependency and Tagged to the React Code
    Added REDUX REACT-REDUX Dependency and Tagged to the React Code

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,30 @@
 import classes from './Counter.module.css';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment'});
+  };
+  
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement'});
+  };
+
+  const toggleCounterHandler = () => {
+
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{ counter }</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Dcrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
